523427b1c7546a7d1743d53259733818b813de63
unknown <daviddimas80@gmail.com>
2022-04-13 11:58:15 +0700

    perubahan struktur

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import Home from './components/home';
 
 function App() {
+  
+  
   return (
-    <div className="App">
-     
-    </div>
+    
+    <Router>
+      
+      <Switch>
+        
+        <Route exact path="/">
+          <Home/>
+        </Route>
+      
+      </Switch>
+      
+    </Router>
+    
   );
 }
 
